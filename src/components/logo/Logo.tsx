20b5a896add530e949af03d7437001e77af16856
John <john.19col@gmail.com>
2020-06-06 21:28:14 -0500

    Fix logo text

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -6,7 +6,7 @@ export const Logo = () => {
   return (
     <div className="logo">
       <span className="logo-m">M</span>
-      <span className="logo-ovib">ovid</span>
+      <span className="logo-ovib">ovib</span>
     </div>
   );
 };
